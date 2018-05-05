b74463d346ec27ee9910e1a91b45d75f36e0eec2
zexhuang <k305261558@icloud.com>
2018-05-04 23:12:36 -0500

    Basic Structure

---
+++ AI_Project_DecisionTree/AI_Project_DecisionTree/ReadFIle.fs
@@ -0,0 +1,19 @@
+﻿// Learn more about F# at http://fsharp.org
+module ReadFile
+open System.IO
+open FSharp.Data
+
+// "/Users/Huangzexian/Projects/parser/res/car.names.json"
+
+let getattributeData(filePath:string) = JsonValue.Load(filePath)
+
+
+// "/Users/Huangzexian/Projects/parser/res/car.data.json"
+
+let getData(filePath:string) = JsonValue.Load(filePath)
+
+
+
+
+
+
